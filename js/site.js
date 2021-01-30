83677b183ab026469dfa36d438811b5519c5ffd7
sempermort <semperjack@gmail.com>
2021-01-31 02:31:39 +0300

    add filter

--- js/site.js
+++ js/site.js
@@ -10,3 +10,18 @@ function openCity(cityName) {
     }
     document.getElementById(cityName).style.display = "block";
 }
+
+//Draggable  slider controller
+  $(function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  });
